55949ec612d6035da495561e0432d74be5eea6d1
RoboStriker2k <rlaimite@gmail.com>
2024-08-14 21:53:34 +0300

    vairaku failu augšupielade+  failu atmešana gaidāma

--- src/app/components/upload.component.ts
+++ src/app/components/upload.component.ts
@@ -2,33 +2,33 @@ import { Component, EventEmitter, Input, Output } from "@angular/core";
 
 @Component({
  selector: "Uploadcomponent",
- template: `  
- <div>
- @if (uploadstatus ) {
-  <div class="upload">
-  
-        
-   
-   <div>
-    <form action="upload"  onSubmit="{(e) => e.preventDefault()}" >
-     <h1>Pievienot</h1>
-     <p>Ieraksta virsraksts</p>
-     <input type="text" id="uploadtitle"  placeholder="Ieraksta virsraksts" />
-     <p>Ieraksta apraksts</p>
-     <input type="text" id="uploadpdesc"  placeholder="Ieraksta apraksts" />
-     <div>
-      <p>Ieraksta attels (var nepievienot)</p>
-      <img id="preview" [src]="" />
-      <input id="fileupl" type="file"  (change)="onFileSelected($event)"/>
-     </div>
-     <div>
-      <button type =button (click)="onUpload()">Upload</button>
-      <button type =button (click)="CLOSBTN()">Aizvert</button>
-     </div>
-    </form>
+ template: `
+  <div>
+   @if (uploadstatus ) {
+   <div class="upload">
+    <div>
+     <form action="upload" onSubmit="{(e) => e.preventDefault()}">
+      <h1>Pievienot</h1>
+      <p>Ieraksta virsraksts</p>
+      <input type="text" id="uploadtitle" placeholder="Ieraksta virsraksts" />
+      <p>Ieraksta apraksts</p>
+      <input type="text" id="uploadpdesc" placeholder="Ieraksta apraksts" />
+      <div>
+       <p>Ieraksta attels -li (var arī nepievienot)</p>
+       @for (img of prviews; track img) {
+       <img id="preview" [src]="img" />
+       }
+       <input id="fileupl" type="file" multiple (change)="onMultipleFilesSelected($event)" />
+      </div>
+      <div>
+       <button type="button" (click)="onUpload()">Upload</button>
+       <button type="button" (click)="CLOSBTN()">Aizvert</button>
+      </div>
+     </form>
+    </div>
    </div>
- 
-  </div>} </div>
+   }
+  </div>
  `,
  standalone: true,
 })
@@ -39,16 +39,20 @@ export class Uploadcomponent {
  uploadtittle: string = "";
  uploaddesc: string = "";
  file: File | null = null;
-
+ files: File[] | null = null;
+ prviews: (string | ArrayBuffer | null)[] = [];
  CLOSBTN() {
+  this.files = null;
+  this.prviews = [];
   this.Changeuploadstatus.emit();
  }
 
  onUpload() {
   let formdata = new FormData();
-  let fileupl = document.getElementById("fileupl") as HTMLInputElement;
-  if (fileupl && fileupl.files && fileupl.files[0]) {
-   formdata.append("file", fileupl.files[0]);
+  if (this.files != null) {
+   for (let i = 0; i < this.files.length; i++) {
+    formdata.append("file", this.files[i]);
+   }
   }
   let ptitle = document.getElementById("uploadtitle") as HTMLInputElement;
   let pdesc = document.getElementById("uploadpdesc") as HTMLInputElement;
@@ -64,21 +68,39 @@ export class Uploadcomponent {
    .catch((error) => {
     console.error("Error:", error);
    });
-
+  this.files = null;
+  this.prviews = [];
   this.Update.emit();
  }
 
  onFileSelected(event: any) {
   this.file = event.target.files[0];
-  if(this.file){
+  if (this.file) {
    let img = document.getElementById("preview") as HTMLImageElement;
-  const reader = new FileReader();
-  reader.onload = (e) => {
- if (e.target){
-   img.src = e.target.result as string ?? "";
-}
-  };
-  reader.readAsDataURL(this.file);
+   const reader = new FileReader();
+   reader.onload = (e) => {
+    if (e.target) {
+     img.src = (e.target.result as string) ?? "";
+    }
+   };
+   reader.readAsDataURL(this.file);
+  }
  }
-}
+
+ onMultipleFilesSelected(event: any) {
+  let files = [];
+
+  for (let i = 0; i < event.target.files.length; i++) {
+   files.push(event.target.files[i]);
+   const reader = new FileReader();
+   reader.onload = (e) => {
+    if (e.target) {
+     (this.prviews.push(e.target.result) as unknown as string) ?? "";
+    }
+   };
+   reader.readAsDataURL(files[i]);
+  }
+
+  this.files = files;
+ }
 }
