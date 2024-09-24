951aca31511e3cc1d8a80d7d5594e9d676d66150
RoboStriker2k <rlaimite@gmail.com>
2024-09-24 12:20:09 +0300

    update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,12 @@
 import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
 import { provideRouter, Routes } from "@angular/router";
+
 export const routes: Routes = [];
 export const appConfig: ApplicationConfig = {
  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)],
 };
 export const serverconfig: configfile = {
- baseurl: "http://localhost:3000",
+ baseurl: `${process.env["baseurl"]}` || "http://localhost:3000",
 };
 export interface configfile {
  baseurl: string;
